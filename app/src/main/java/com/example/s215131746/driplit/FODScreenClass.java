eb1c0776448d7a13ee27939d273138b606151747
RouxJr <anathi.roux@gmail.com>
2018-05-12 08:29:46 +0200

    Screens need A LOT! of work Main Menu does not have a button and Intents linked to them.. attributes might cause some problems.

--- app/src/main/java/com/example/s215131746/driplit/FODScreenClass.java
+++ app/src/main/java/com/example/s215131746/driplit/FODScreenClass.java
@@ -1,6 +1,8 @@
 package com.example.s215131746.driplit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +13,7 @@ import android.view.ViewGroup;
  */
 
 public class FODScreenClass extends Fragment {
+    Intent mainMenu;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_fodscreen, container, false);
