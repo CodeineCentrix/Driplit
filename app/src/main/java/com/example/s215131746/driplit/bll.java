0742eb306ecf4002eabf6ae2c63e8d250c1939e6
RouxJr <anathi.roux@gmail.com>
2018-05-24 12:55:10 +0200

    trends on the graph new saving

--- app/src/main/java/com/example/s215131746/driplit/bll.java
+++ app/src/main/java/com/example/s215131746/driplit/bll.java
@@ -1,23 +1,42 @@
 package com.example.s215131746.driplit;
 
+import android.os.StrictMode;
+
+import java.net.ConnectException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 /**
  * Created by s216127904 on 2018/05/01.
  */
 
 public class bll {
+
+    private Connection connection;
+    private Statement statement;
+    private ResultSet resultSet;
+    private String errorMassage;
+    String[] ItemNames;
+    String[] Averages;
+    public bll()
+    {
+        Select();
+    }
     public String[] getItemName()
     {
-        String[] ItemName = {"Bath","Shower","Toilet","Garden","Dish Washer","Washing Machine","Brush teeth"};
-        return ItemName;
+        return ItemNames;
     }
     public String[] getItemAverageUse()
     {
-        String[] Avg = {"5","6","2","2","5","8","1"};
-        return  Avg;
+        return  Averages;
     }
     public int[] getItemIcon()
     {
-        int[] icon = {5,6,2,2,5,8,1};
+        int[] icon = {5,6,2,2,5,8,15,6,2,2,5,8,15,6,2,2,5,8,15,6,2,2,5,8,15,6,2,2,5,8,1};
         return  icon;
     }
     public String getPassword()
@@ -27,6 +46,62 @@ public class bll {
     public String getUsername()
     {
         return  null;
+    }
+    private void Connect()
+    {
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+
+            Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
+            String user = "codecentrix";
+             String password = "password";
+            connection = DriverManager.getConnection("jdbc:jtds:sqlserver://10.103.137.7:1433/Codecentrix" , user, password);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private void Select()
+    {
+        Connect();
+        try
+        {
+
+            Statement st = connection.createStatement();
+            resultSet = st.executeQuery("SELECT * FROM WaterUsageItem;");
+        }
+        catch (SQLException e)
+        {
+        }
+        ArrayList<String> name = new ArrayList<>();
+        ArrayList<String> Avg = new ArrayList<>();
+
+        try {
+            while(resultSet.next())
+            {
+
+                name.add(resultSet.getString("ItemDescription").toString());
+                Avg.add(""+resultSet.getFloat("ItemAverageAmount"));
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        String[] ItemName = new String[name.size()];
+        ItemNames = name.toArray(ItemName);
+        String[] Average = new String[Avg.size()];
+        Averages = Avg.toArray(Average);
+
     }
 
 }
