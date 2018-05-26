31ec2f8aff79d87a0560337cee5eafcb582b6e21
RouxJr <anathi.roux@gmai.com>
2018-05-26 12:49:22 +0200

    old thing i dont want to be saved but i want to update they wont allow untill i commit

--- app/src/main/java/com/example/s215131746/driplit/ReportLeakClass.java
+++ app/src/main/java/com/example/s215131746/driplit/ReportLeakClass.java
@@ -1,10 +1,13 @@
 package com.example.s215131746.driplit;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 /**
  * Created by s216127904 on 2018/04/30.
@@ -14,7 +17,15 @@ public class ReportLeakClass extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.report_leak, container, false);
-
+        ImageButton imgReport = rootView.findViewById(R.id.imageButton);
+        bll b = new bll();
+        imgReport.setImageBitmap(b.ScaleImg(R.drawable.tontsi,getResources()));
+        imgReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"Leak reported",Toast.LENGTH_LONG);
+            }
+        });
         return rootView;
     }
 
--- app/src/main/java/com/example/s215131746/driplit/bll.java
+++ app/src/main/java/com/example/s215131746/driplit/bll.java
@@ -1,5 +1,9 @@
 package com.example.s215131746.driplit;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 /**
  * Created by s216127904 on 2018/05/01.
  */
@@ -27,6 +31,42 @@ public class bll {
     public String getUsername()
     {
         return  null;
+    }
+    public Bitmap ScaleImg(int pic, Resources res)
+    {
+        Bitmap scaledImg;
+        BitmapFactory.Options op = new BitmapFactory.Options();
+
+        op.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res,pic,op);
+
+        int imgWidth = op.outWidth;
+        if(imgWidth>1500)
+        {
+            op.inSampleSize = 20;
+        }
+        else if(imgWidth>500)
+        {
+            op.inSampleSize = 5;
+        }
+        else if(imgWidth>400)
+        {
+            op.inSampleSize = 4;
+        }
+        else if(imgWidth>300)
+        {
+            op.inSampleSize = 3;
+        }
+        else
+        {
+            op.inSampleSize = 2;
+        }
+
+
+        op.inJustDecodeBounds = false;
+        scaledImg = BitmapFactory.decodeResource(res,pic,op);
+
+        return scaledImg;
     }
 
 }
